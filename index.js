dc2d806d5b7bac905286e76ea87984a5642b0dcc
Felipe Guimarães <felipege25@gmail.com>
2022-06-15 14:42:18 -0300

    Finalizado API

---
+++ index.js
@@ -0,0 +1,35 @@
+// config inicial
+const express = require("express")
+const mongoose = require("mongoose")
+require('dotenv').config()
+const app = express()
+
+// forma de ler JSON
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+)
+
+app.use(express.json())
+
+//rotas da API
+const personRoutes = require("./routes/personRoutes")
+
+app.use("/person", personRoutes)
+
+// rota inicial / endpoint
+app.get("/", (req, res) => {
+    // mostrar req
+    
+    res.json({ message: "Oi Express!"})
+})
+
+// entregar uma porta
+mongoose.connect()
+
+.then(() => {
+    console.log("Conectamos ao MongoDB!")
+    app.listen(3000)
+})
+.catch((err) => console.log(err))
